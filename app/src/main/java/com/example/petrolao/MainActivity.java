60428554f313563eae21d51a168dec737e82c48d
lguifcosta <lgcosta.lgfc@gmail.com>
2023-09-21 16:49:01 -0300

    aplicação das primeiras formulas

--- app/src/main/java/com/example/petrolao/MainActivity.java
+++ app/src/main/java/com/example/petrolao/MainActivity.java
@@ -3,33 +3,141 @@ package com.example.petrolao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
-import java.util.EnumSet;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText litros;
     private Spinner combustiveis;
+    private EditText precoComb;
+    private TextView precoFinal;
+    private Button pagar;
+    private int selected;
+    private RadioGroup payGroup;
+    private double desconto = 0.95;
+    private double[] precos = {5.84,5.70,9.60,3.59,6.20};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(this /* MyActivity */, text, duration);
+
+        //variaveis do front
         litros = (EditText) findViewById(R.id.litros);
+
         combustiveis = (Spinner) findViewById(R.id.combustiveis);
 
 
+        payGroup = (RadioGroup) findViewById(R.id.payGroup);
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
+        precoComb = (EditText) findViewById(R.id.precoComb);
+
+        precoFinal = (TextView) findViewById(R.id.precoFinal);
+
+        pagar = (Button) findViewById(R.id.pagar);
+
+
+
+        selected = 0;
+
+        //configuração do spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.array_combustiveis, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
         combustiveis.setAdapter(adapter);
 
+        // Cria uma classe interna que implementa a interface OnItemSelectedListener
+        class MyListener implements AdapterView.OnItemSelectedListener{
+
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Executa uma função ao mudar o spinner
+                selected = position;
+                switch (position){
+                    case 0:
+                        precoComb.setText(Double.toString(precos[selected]));
+
+                        break;
+                    case 1:
+                        precoComb.setText(Double.toString(precos[selected]));
+                        break;
+                    case 2:
+                        precoComb.setText(Double.toString(precos[selected]));
+                        break;
+                    case 3:
+                        precoComb.setText(Double.toString(precos[selected]));
+                        break;
+                    case 4:
+                        precoComb.setText(Double.toString(precos[selected]));
+                        break;
+                }
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Executa a função quando o spinner não for selecionado
+                //do nothing
+            }
+        }
+
+        // Associa o listener ao spinner
+        combustiveis.setOnItemSelectedListener(new MyListener());
+        precoComb.setText(Double.toString(precos[0]));
+
+        payGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+                String option = radioButton.getText().toString();
+
+
+                switch (option){
+                    case "pix":
+                        desconto = 1.05;
+                        toast.show();
+                        break;
+                    case "dinheiro":
+                        desconto = 0.95;
+                        break;
+                    case "debito":
+                        desconto = 1.05;
+                        toast.show();
+                        break;
+                    case "credito":
+                        desconto = 1.1;
+                }
+            }
+        });
+
+    }
+
+
+    public void pagamento( View view ){
+        //finalização e calculo da transação
+        double x, y, result;
+
+        x = Double.valueOf(litros.getText().toString());
+        y = Double.valueOf(precoComb.getText().toString());
+
+        precos[selected]=y;
+
+        result = x * y;
+        result *= desconto;
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        precoFinal.setText("R$:"+Double.toString(result));
     }
 
 }
